c811e9656146c2c9f67e4e9b3450e61f6000eb92
Danny-Chung09 <dchung6969@gmail.com>
2023-02-22 17:05:33 -0800

    updated swap and added temp swap particle system

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,143 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    public GameObject character;
+    public CameraMovement cameraMovement;
+
+    //A list of prefabs of the characters. This is possible since we will be keeping the information that
+    //has to be persistent in this script.
+    public List<GameObject> CharacterList;
+
+    //A dictionary of the character names, and their health value. Since each character will have a
+    //unique name, we can always know how much health a character has, and we can always access their values.
+    public Dictionary<string, float> CharacterHealths;
+
+    //current character
+    public int whichCharacter;
+
+    //particle system
+    public ParticleSystem m_ParticleSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //setting character to the first character on startup
+        if (character == null && CharacterList.Count >= 1) {
+            character = Instantiate(CharacterList[0], this.transform.position, this.transform.rotation);
+        }
+        if (CharacterList.Count >= 1) {
+            for (int i = 0; i < CharacterList.Count; i++) {
+                if (i != whichCharacter) {
+                   CharacterList[i].SetActive(false); 
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //going left through the list
+        if (Input.GetKeyDown(KeyCode.Z)){
+            PreviousCharacter();
+        }
+        //going right through the list
+        if (Input.GetKeyDown(KeyCode.X)){
+            NextCharacter();
+        }
+        
+        //swap to 0
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            Swap(0);
+            //PreviousCharacter();
+        }
+
+        //swap to 1
+        if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            Swap(1);
+            //NextCharacter();
+        }
+    }
+    //Health interaction functions
+    public float GetHealth(){
+        //return CharacterHealths[character.GetComponent<Character>().name];
+        return 0;
+    }
+    public float GetHealth(string charName){
+        return CharacterHealths[charName];
+    }
+
+
+    //damage to deal to the player
+    public void Damage(int damage){
+        // CharacterHealths[character.GetComponent<Character>().name] -= damage;
+        // if (CharacterHealths[character.GetComponent<Character>().name] < 0){
+        //     //character died / incapacitated
+        // }
+    }
+    public void Damage(int damage, string charName){
+        CharacterHealths[charName] -= damage;
+        if (CharacterHealths[charName] < 0){
+            //character died / incapacitated
+        }
+    }
+
+    //Character swapping functions
+    public void PreviousCharacter(){
+        //decrement the whichCharacter, with wraparound
+        //Debug.Log("prev");
+        int temp = whichCharacter;
+        if (whichCharacter == 0) {
+            whichCharacter = CharacterList.Count - 1;
+        } else {
+            whichCharacter -= 1;
+        }
+        if (temp == whichCharacter) return; // swapping to the same character
+        Swap(whichCharacter);
+    }
+    public void NextCharacter(){
+        //increment the whichCharacter, with wraparound
+        //Debug.Log("next");
+        int temp = whichCharacter;
+        if (whichCharacter == CharacterList.Count - 1) {
+            whichCharacter = 0;
+        } else {
+            whichCharacter += 1;
+        }
+        if (temp == whichCharacter) return; // swapping to the same character
+        //Debug.Log("next is " + whichCharacter);
+        Swap(whichCharacter);
+    }
+
+    public void Swap(int index) {
+        //Debug.Log("called " + index);
+        if (index < 0 || index > CharacterList.Count - 1) return; // bounds check
+        //Debug.Log("checked bounds");
+        if (whichCharacter == index) return; // swapping to the same character
+        whichCharacter = index;
+        //keep track of where the player currently is and looking
+        Vector3 currentposition = character.transform.position;
+        Vector3 currentEulerAngle = character.transform.eulerAngles;
+        //disables every character besides the current one
+        for (int i = 0; i < CharacterList.Count; i ++) {
+            if (i != whichCharacter) {
+                CharacterList[i].SetActive(false);
+            }
+        }
+        CharacterList[whichCharacter].SetActive(true);
+        character = (CharacterList[whichCharacter]);
+        character.transform.position = currentposition;
+        character.transform.eulerAngles = currentEulerAngle;
+        m_ParticleSystem.transform.position = currentposition;
+        m_ParticleSystem.Play();
+        cameraMovement.PlayerTransform = character.transform;
+
+        // if (character != null) Destroy(character);
+        // character = Instantiate(CharacterList[whichCharacter]);
+        // cameraMovement.PlayerTransform = character.transform;
+        //character.GetComponent<ThirdPersonMovement>().enabled = true;
+    }
+}
